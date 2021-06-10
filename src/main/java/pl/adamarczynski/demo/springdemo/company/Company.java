ce461b5acdeff5c994e7464e604a5b9d6c337234
Adam Arczynski <adam.arczynski@gmail.com>
2021-06-10 12:46:25 +0200

    improved logging

--- src/main/java/pl/adamarczynski/demo/springdemo/company/Company.java
+++ src/main/java/pl/adamarczynski/demo/springdemo/company/Company.java
@@ -8,13 +8,9 @@ import pl.adamarczynski.demo.springdemo.company.department.DepartmentCost;
 import pl.adamarczynski.demo.springdemo.company.department.DepartmentNotFoundException;
 import pl.adamarczynski.demo.springdemo.company.department.DepartmentRepository;
 import pl.adamarczynski.demo.springdemo.company.employee.Employee;
-import pl.adamarczynski.demo.springdemo.company.employee.EmployeeRepository;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import static java.math.BigDecimal.ZERO;
 import static java.util.stream.Collectors.toList;
@@ -50,7 +46,7 @@ public class Company {
 
     public DepartmentCost findDepartmentCost(@PathVariable String departmentName) {
         var department = departmentRepository.findByNameIgnoreCase(departmentName)
-                .orElseThrow(() -> new DepartmentNotFoundException("not found"));
+                .orElseThrow(() -> new DepartmentNotFoundException(departmentName + " department not found"));
 
         var employees = department.getEmployees();
         var cost = employees
@@ -58,7 +54,7 @@ public class Company {
                 .map(Employee::getSalary)
                 .reduce(ZERO, BigDecimal::add);
 
-        log.info("Found total cost of {} {} employees equal to {}", employees.size(), departmentName, cost);
+        log.info("Found total cost of {} {} employees equal to {}", employees.size(), department.getName(), cost);
         return new DepartmentCost(department.getName(), cost);
     }
 }
--- src/main/java/pl/adamarczynski/demo/springdemo/company/CompanyController.java
+++ src/main/java/pl/adamarczynski/demo/springdemo/company/CompanyController.java
@@ -1,16 +1,22 @@
 package pl.adamarczynski.demo.springdemo.company;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import pl.adamarczynski.demo.springdemo.company.department.DepartmentCost;
 import pl.adamarczynski.demo.springdemo.company.department.DepartmentNotFoundException;
 
-import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/company/departments")
 public class CompanyController {
@@ -23,12 +29,14 @@ public class CompanyController {
 
     @GetMapping("/cost")
     public List<DepartmentCost> allCosts() {
+        log.info("Received request for cost of all departments");
         return company.findAllCosts();
     }
 
     @GetMapping("/{departmentName}/cost")
     public DepartmentCost allCosts(@PathVariable String departmentName) {
-            return company.findDepartmentCost(departmentName);
+        log.info("Received request for cost of {} department", departmentName);
+        return company.findDepartmentCost(departmentName);
     }
 
     @RestControllerAdvice
@@ -36,6 +44,7 @@ public class CompanyController {
         @ExceptionHandler(DepartmentNotFoundException.class)
         @ResponseStatus(HttpStatus.NOT_FOUND)
         public HashMap<String, String> handleDepartmentNotFound(DepartmentNotFoundException e, WebRequest r) {
+            log.error("Requested department not found - returning 404", e);
             return buildErrorResponse(e);
         }
 
