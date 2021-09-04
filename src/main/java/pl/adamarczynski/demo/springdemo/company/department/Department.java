bccca80fac1ac384f00ddb1f4cf4d182336d9817
Adam Arczynski <adam.arczynski@gmail.com>
2021-09-04 12:34:58 +0200

    instructed hibernate to use subselect to fix n+1 problem

--- src/main/java/pl/adamarczynski/demo/springdemo/company/department/Department.java
+++ src/main/java/pl/adamarczynski/demo/springdemo/company/department/Department.java
@@ -3,6 +3,8 @@ package pl.adamarczynski.demo.springdemo.company.department;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import pl.adamarczynski.demo.springdemo.company.employee.Employee;
 
 import javax.persistence.*;
@@ -21,6 +23,7 @@ public class Department {
     private String name;
     @ToString.Exclude
     @OneToMany(mappedBy = "department", fetch = FetchType.LAZY)
+    @Fetch(value = FetchMode.SUBSELECT)
     private List<Employee> employees;
 
 }
